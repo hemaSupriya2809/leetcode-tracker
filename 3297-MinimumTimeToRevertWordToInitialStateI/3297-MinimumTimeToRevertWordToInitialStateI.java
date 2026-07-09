c46bef3c42bfa804558e78b3f196a01398a42055
hemaSupriya2809 <hemasupriyat@gmail.com>
2026-07-09 09:17:03 +0530

    Create 3297-MinimumTimeToRevertWordToInitialStateI

---
+++ 3297-MinimumTimeToRevertWordToInitialStateI/3297-MinimumTimeToRevertWordToInitialStateI.java
@@ -0,0 +1,19 @@
+// Last updated: 7/9/2026, 9:17:04 AM
+class Solution {
+    public int minimumTimeToInitialState(String word, int k) {
+        int count = 1;
+        for(int i=k; i<word.length(); i+=k){
+            if(checkPrefix(word, i))
+                return count;
+            count++;
+        }
+        return count;
+    }
+    private boolean checkPrefix(String word, int idx){
+        for(int i=idx; i<word.length(); i++){
+            if(word.charAt(i-idx) != word.charAt(i))
+                return false;
+        }
+        return true;
+    }
+}
