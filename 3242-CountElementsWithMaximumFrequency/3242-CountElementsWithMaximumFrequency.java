2250e5dfcc06e22d22dbb383008c9660985f1370
hemaSupriya2809 <hemasupriyat@gmail.com>
2026-07-09 09:17:09 +0530

    Create 3242-CountElementsWithMaximumFrequency

---
+++ 3242-CountElementsWithMaximumFrequency/3242-CountElementsWithMaximumFrequency.java
@@ -0,0 +1,24 @@
+// Last updated: 7/9/2026, 9:17:11 AM
+class Solution {
+    public int maxFrequencyElements(int[] nums) {
+        int a[]=new int[101];
+        for(int i:nums)
+        {
+            a[i]++;
+        }
+        int maxi=0;
+        for(int i=0;i<=100;i++)
+        {
+            maxi=Math.max(maxi,a[i]);
+        }
+        int ans=0;
+        for(int i=0;i<=100;i++)
+        {
+            if(a[i]==maxi)
+            {
+                ans+=maxi;
+            }
+        }
+        return ans;
+    }
+}
