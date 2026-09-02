31c2fbc36dadc600ee3c8f02be46c6556ea8face
hemaSupriya2809 <hemasupriyat@gmail.com>
2026-09-02 10:03:40 +0530

    Create 260-SingleNumberIii

---
+++ 260-SingleNumberIii/260-SingleNumberIii.java
@@ -0,0 +1,24 @@
+// Last updated: 9/2/2026, 10:03:40 AM
+class Solution {
+    public int[] singleNumber(int[] nums) {
+        int n = nums.length;
+        int[] result = new int[2];
+        int index = 0;
+        for (int i = 0; i < n; i++) {
+            boolean found = false;
+            for (int j = 0; j < n; j++) {
+                if (i != j && nums[i] == nums[j]) {
+                    found = true;
+                    break;
+                }
+            }
+            if (!found) {
+                result[index++] = nums[i];
+                if (index == 2) {
+                    break;
+                }
+            }
+        }
+        return result;
+    }
+}
