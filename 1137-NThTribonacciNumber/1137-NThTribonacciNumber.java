6609260578dc77bb6c297309333a0abf2e50b5cf
Hema Supriya T <hemasupriyat@gmail.com>
2026-09-12 09:34:40 +0530

    Create 1137-NThTribonacciNumber

---
+++ 1137-NThTribonacciNumber/1137-NThTribonacciNumber.java
@@ -0,0 +1,12 @@
+// Last updated: 9/12/2026, 9:34:39 AM
+class Solution {
+    Map<Integer , Integer> dp = new HashMap<>();
+    public int tribonacci(int n) {
+        if(n==0) return 0;
+        if(n==1 || n==2) return 1;
+        if(dp.containsKey(n)) return dp.get(n);
+        int nt = tribonacci(n-1) + tribonacci(n-2) + tribonacci(n-3);
+        dp.put(n,nt);
+        return nt;
+    }
+}
