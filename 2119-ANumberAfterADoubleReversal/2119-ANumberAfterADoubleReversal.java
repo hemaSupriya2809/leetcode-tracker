735f825955b17ff6fa5add522985356c70c14690
Hema Supriya T <hemasupriyat@gmail.com>
2026-09-12 09:33:55 +0530

    Create 2119-ANumberAfterADoubleReversal

---
+++ 2119-ANumberAfterADoubleReversal/2119-ANumberAfterADoubleReversal.java
@@ -0,0 +1,8 @@
+// Last updated: 9/12/2026, 9:33:55 AM
+class Solution {
+    public boolean isSameAfterReversals(int num) {
+     if(num==0)return true;  
+     if(num%10==0) return false;
+     else return true; 
+    } 
+}
