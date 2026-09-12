138683293f2df2261c08d75b89c5d4da2d73f5d8
Hema Supriya T <hemasupriyat@gmail.com>
2026-09-12 09:34:10 +0530

    Create 1672-RichestCustomerWealth

---
+++ 1672-RichestCustomerWealth/1672-RichestCustomerWealth.java
@@ -0,0 +1,16 @@
+// Last updated: 9/12/2026, 9:34:09 AM
+class Solution {
+    public int maximumWealth(int[][] accounts) {
+        int max=0;
+        for(int i=0;i<accounts.length;i++){
+            int sum=0;
+            for(int j=0;j<accounts[i].length;j++){
+                sum=sum+accounts[i][j];
+            }
+            if(sum>max){
+                max=sum;
+            }
+        }
+        return max;
+    }
+}
