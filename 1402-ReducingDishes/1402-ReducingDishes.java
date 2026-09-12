e501cd81859d7c1f0249e3a155b13f3c98d1a57d
Hema Supriya T <hemasupriyat@gmail.com>
2026-09-12 09:34:20 +0530

    Create 1402-ReducingDishes

---
+++ 1402-ReducingDishes/1402-ReducingDishes.java
@@ -0,0 +1,17 @@
+// Last updated: 9/12/2026, 9:34:19 AM
+class Solution {
+    public int maxSatisfaction(int[] satisfaction) {
+        Arrays.sort(satisfaction);
+
+        int prefSum = 0, res = 0, n = satisfaction.length;
+        for (int i = n - 1; i >= 0; i--) {
+            prefSum += satisfaction[i];
+            if (prefSum < 0) {
+                break;
+            }
+            res += prefSum;
+        }
+        
+        return res;
+    }
+}
