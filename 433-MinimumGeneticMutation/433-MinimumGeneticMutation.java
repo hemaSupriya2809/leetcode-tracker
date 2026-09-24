9a1b722031383e8b6992d785c7a1d41a779aebfe
Hema Supriya T <hemasupriyat@gmail.com>
2026-09-24 12:28:08 +0530

    Create 433-MinimumGeneticMutation

---
+++ 433-MinimumGeneticMutation/433-MinimumGeneticMutation.java
@@ -0,0 +1,43 @@
+// Last updated: 9/24/2026, 12:28:07 PM
+class Solution {
+    public int minMutation(String startGene, String endGene, String[] bank) {
+        Set<String> geneBank = new HashSet<>(Arrays.asList(bank));
+        if (!geneBank.contains(endGene)) {
+            return -1;
+        }
+
+        char[] mutations = {'A', 'C', 'G', 'T'};
+        int mutationCount = 0;
+        Queue<String> queue = new LinkedList<>();
+        queue.add(startGene);
+        while (!queue.isEmpty()) {
+            int levelSize = queue.size();
+            
+            for (int i = 0; i < levelSize; i++) {
+                String gene = queue.poll();
+                
+                if (gene.equals(endGene)) {
+                    return mutationCount;
+                }
+                StringBuilder currGene = new StringBuilder(gene);
+                for (int index = 0; index < 8; index++) {
+                    char original = currGene.charAt(index);
+                    
+                    for (char mutation : mutations) {
+                        currGene.setCharAt(index, mutation);
+                        String nextGene = currGene.toString();
+                        
+                        if (geneBank.contains(nextGene)) {
+                            queue.add(nextGene);
+                            geneBank.remove(nextGene); 
+                        }
+                    }
+                    currGene.setCharAt(index, original); 
+                }
+            }
+            mutationCount++; 
+        }
+
+        return -1; 
+    }
+}
